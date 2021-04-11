17e5d6f7691d09d0c78b5ec83cdf9ef3b6b91995
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-04-10 20:52:20 -0700

    first commit

---
+++ servidor.php
@@ -0,0 +1,7 @@
+<?php
+$tecnologias = array(
+    'frontend' => 'html, css3,javascript',
+    'backend' => 'php, mysql,apache',
+    'fullstack' => 'html, css3,javascript,php, mysql,apache'
+);
+echo json_encode($tecnologias);
